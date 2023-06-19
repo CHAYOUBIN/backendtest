ba26046c782646b10a5185bfaa23c9e443a63996
user <user@DESKTOP171007>
2023-06-19 16:35:33 +0900

    z

---
+++ naverai_boot/src/main/java/com/example/ai/MyNaverInform.java
@@ -0,0 +1,21 @@
+package com.example.ai;
+
+public class MyNaverInform {
+	public static final String clientID = "ozm6wnniaz";
+	public static final String secret = "AXCu79yyEzQfqfbl2FCJsET4DnUT4ukB8T1yZj9D";
+	public static final String path = "C:\\Users\\user\\Desktop\\네이버플랫폼\\ai_images";
+	
+	//clova voice(tts)시 사용
+	public static final String voice_clientID = "ozm6wnniaz";
+	public static final String voice_secret = "AXCu79yyEzQfqfbl2FCJsET4DnUT4ukB8T1yZj9D";
+	
+	//ocr 사용
+	public static final String ocrsecret = "SXdTeUhmSEFMTHNkWkhHTGxVV1lCaFFXTGtuSVBVeng=";
+	public static final String apiURL = "https://645wclj2l2.apigw.ntruss.com/custom/v1/23052/798b8f0a3464b21a455d13e099eba48babb4eb49a030d8a8a70f2612c02e24db/general";
+
+	//chatbot
+	public static final String chatbot_apiURL=
+			"https://zenr15lk3x.apigw.ntruss.com/custom/v1/10750/cb2ab0bee457196e1855bae09387ead0f5bfa57fc8a24e0dd1cfb8ed3a54908d";
+	public static final String chatbot_sercet=
+			"enFucHJWTXF1S2lZRENvdUVvam9MZ2JDR2lkRE1Uc1Y=";
+}
